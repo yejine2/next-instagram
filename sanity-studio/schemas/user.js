400bdf52b331eadd428cfa9cbd9a9fbf05d31e56
yejine2 <yejine2@naver.com>
2023-10-10 15:45:56 +0900

    feat: Add user schema

---
+++ sanity-studio/schemas/user.js
@@ -0,0 +1,67 @@
+// https://www.sanity.io/docs/schema-types
+export default {
+  title: 'User', // sanify studio UI상의 이름
+  name: 'user',
+  type: 'document',
+  fields: [
+    // id 개념
+    {
+      title: 'Username',
+      name: 'username', // DB 접근 이름, key,
+      type: 'string', // name 데이터 타입은 string
+    },
+    // 유저의 실제 이름
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'string',
+    },
+    {
+      title: 'Email',
+      name: 'email',
+      type: 'string',
+    },
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'string',
+    },
+    {
+      title: 'Following',
+      name: 'following',
+      type: 'array',
+      of: [
+        // 배열인데 user를 참고하는 배열
+        {
+          type: 'reference',
+          to: [{type: 'user'}],
+        },
+      ],
+      validation: (Rule) => Rule.unique(), // 중복제거
+    },
+    {
+      title: 'Followers',
+      name: 'followers',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'user'}],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+    {
+      title: 'Bookmarks',
+      name: 'bookmarks',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'post'}],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+  ],
+}
