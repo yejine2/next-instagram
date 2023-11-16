c0b3c47663b39096bd1e4112dc8b1628b598d903
yejine2 <yejine2@naver.com>
2023-11-16 16:06:37 +0900

    feat: Update HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <h1 className="text-gray-500">Instagram</h1>;
+import FollowingBar from "@/components/FollowingBar";
+import PostList from "@/components/PostList";
+import SideBar from "@/components/SideBar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function HomePage() {
+  // 페이지는 즉, 서버 컴포넌트
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/auth/signin");
+  }
+
+  // 클라이언트 컴포넌트로 정보 props
+  return (
+    <section className="w-full flex flex-col md:flex-row max-w-[850px] p-4">
+      <div className="w-full basis-3/4">
+        <FollowingBar />
+        <PostList />
+      </div>
+      <div className="basis-1/4">
+        {/* 정적 서버 컴포넌트 */}
+        <SideBar user={user} />
+      </div>
+    </section>
+  );
 }
