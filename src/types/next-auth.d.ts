15596033a0c09421adcd0f5d95179fe8ba377e6a
yejine2 <yejine2@naver.com>
2023-11-16 16:06:21 +0900

    feat: Update type

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,11 +1,13 @@
-import NextAuth, { DefaultSession } from "next-auth/next";
+import { User } from "@/model/user";
+// import NextAuth, { DefaultSession } from "next-auth/next";
 
 // 기존 next-auth의 타입 중 interface Session에 user의 username라는 임의의 type 추가
 // DefaultSession["user"] 타입 그대로 가져오고 거기에 username type 추가
 declare module "next-auth" {
   interface Session {
-    user: {
-      username: string;
-    } & DefaultSession["user"];
+    // user: {
+    //   username: string;
+    // } & DefaultSession["user"];
+    user: User; // 직접 정의한 타입으로 재정의
   }
 }
