420b654ae1c9e092612d8f3e099fe4290c89b8c5
yejine2 <yejine2@naver.com>
2023-11-17 17:55:39 +0900

    feat: Create posts api route

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,16 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { getFollowingPostsOf } from "@/service/posts";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response("Authentication Error", { status: 401 }); // 401: Unauthorized error
+  }
+  return getFollowingPostsOf(user.username).then((data) =>
+    NextResponse.json(data)
+  );
+}
---
+++ src/service/posts.ts
@@ -0,0 +1,24 @@
+import { client } from "./sanity";
+
+// projection 하면서 필드 추가 하는 것
+// post.author.username -> post.username 으로 간편하게 사용할 수 있게 풀어서
+// author->username는, author에 있는 username을 "username" 키에 할당한다는 뜻
+const simplePostProjection = `
+...,
+"username": author->username,
+"userImage":  author->image,
+"image": photo,
+"likes": likes[]->username,
+"text": comments[0].comment,
+"comments": count(comments),
+"id":_id,
+"createdAt":_createdAt
+`;
+
+export async function getFollowingPostsOf(username: string) {
+  return client.fetch(/* groq */ `*[_type == "post" && author->username == "${username}"
+  ||  author._ref in *[_type == "user" && username == "${username}"].following[]._ref] | order(_createdAt desc){
+    ${simplePostProjection}
+  }
+    `);
+}
