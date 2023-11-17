524e1c883c60d62a73b48ff3cc9ea5f40663e816
yejine2 <yejine2@naver.com>
2023-11-17 17:57:36 +0900

    feat: Remove unused parameter

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -5,7 +5,7 @@ import { getUserByUsername } from "@/service/user";
 
 // 로직을 한 곳에 다 정리하지마라.
 // Route요청은 이 메서드 즉 GET요청에 대한 유효한 인자들이 다 전달되었는지 확인한 다음 요청은 전가하고 모듈별, 책임별, 함수별로 쪼개어서 작성하는 것이 좋음
-export async function GET(request: Request) {
+export async function GET() {
   // 1. 클라이언트가 request로 요청한 헤더의 쿠키에 있는 jwt정보를 근거로해서, 사용자가 누구인지 파악
   // 2. next.auth가 알아서 해줌 - getServerSession() 사용하면 됨
   const session = await getServerSession(authOptions);
