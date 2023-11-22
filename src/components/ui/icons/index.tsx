e85034eb80c011255f66867a705502aa9363b5fc
yejine2 <yejine2@naver.com>
2023-11-22 11:41:28 +0900

    feat: Add icons

--- src/components/ui/icons/index.tsx
+++ src/components/ui/icons/index.tsx
@@ -5,3 +5,6 @@ export { default as SearchIcon } from "./SearchIcon";
 export { default as SearchFillIcon } from "./SearchFillIcon";
 export { default as NewIcon } from "./NewIcon";
 export { default as NewFillIcon } from "./NewFillIcon";
+export { default as BookmarkIcon } from "./BookmarkIcon";
+export { default as HeartIcon } from "./HeartIcon";
+export { default as SmileIcon } from "./SmileIcon";
