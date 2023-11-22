29dad99afdf6321186dd8a30388b6fe69be90487
yejine2 <yejine2@naver.com>
2023-11-22 12:13:38 +0900

    style: Update style

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,10 +1,11 @@
+type AvatarSize = "small" | "medium" | "large";
 type Props = {
   image?: string | null;
-  size?: "small" | "normal";
+  size?: AvatarSize;
   highlight?: boolean;
 };
 
-function Avatar({ image, size = "normal", highlight = false }: Props) {
+function Avatar({ image, size = "large", highlight = false }: Props) {
   //  image: 'https://lh3.googleusercontent.com/a/ACg8ocLKtre0JW04Xi8lSoO7dtwbIOayf7oF8nBwDAvV5XUzrY0=s96-c'
   // 외부 서버에서 가져오는 이미지는 호스트가 달라질 수 있으므로 기존 img 사용
   return (
@@ -12,7 +13,9 @@ function Avatar({ image, size = "normal", highlight = false }: Props) {
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         referrerPolicy="no-referrer"
-        className="rounded-full p-[0.1rem] w-full h-full"
+        className={`bg-white object-cover rounded-full ${getImageSizeStyle(
+          size
+        )}`}
         alt="user profile"
         src={image ?? undefined}
       />
@@ -22,13 +25,34 @@ function Avatar({ image, size = "normal", highlight = false }: Props) {
 
 export default Avatar;
 
-function getContainerStyle(size: string, highlight: boolean): string {
-  // w-9 h-9 rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300
-  const baseStyle = "rounded-full";
+// 조건부 스타일
+function getContainerStyle(size: AvatarSize, highlight: boolean): string {
+  const baseStyle = "rounded-full flex justify-center items-center";
   const highlightStyle = highlight
     ? "bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300"
     : "";
-  const sizeStyle = size === "small" ? "w-9 h-9" : "w-[68px] h-[68px]";
-
+  const sizeStyle = getContainerSize(size);
   return `${baseStyle} ${highlightStyle} ${sizeStyle}`;
+}
+
+function getContainerSize(size: AvatarSize): string {
+  switch (size) {
+    case "small":
+      return "w-9 h-9";
+    case "medium":
+      return "w-11 h-11";
+    case "large":
+      return "w-[68px] h-[68px]";
+  }
+}
+
+function getImageSizeStyle(size: AvatarSize): string {
+  switch (size) {
+    case "small":
+      return "w-[34px] h-[34px] p-[0.1rem]";
+    case "medium":
+      return "w-[42px] h-[42px] p-[0.1rem]";
+    case "large":
+      return "w-16 h-16 p-[0.2rem]";
+  }
 }
