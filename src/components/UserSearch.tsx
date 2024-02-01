972acaf19c99ccaa28663892ffd18e8a53973304
yejine2 <yejine2@naver.com>
2024-02-01 14:26:50 +0900

    feat: Add debounce search

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,15 +1,49 @@
 "use client";
 
-import { useState } from "react";
+import { ProfileUser } from "@/model/user";
+import { FormEvent, useState } from "react";
 import useSWR from "swr";
+import UserCard from "./UserCard";
+import { BeatLoader } from "react-spinners";
+import useDebounce from "@/hooks/debounce";
 
 export default function UserSearch() {
-  // /api/search/${keyword}
-  // 검색하는 keyword가 있다면 /api/search/bob -> 유저네임이나, 네임
-  // 검색하는 keyword가 없다면 /api/search -> 전제 유저
   const [keyword, setKeyword] = useState("");
-  const { data, isLoading, error } = useSWR(`/api/search/${keyword}`);
-  console.log(data);
+  const debouncedKeyword = useDebounce(keyword);
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useSWR<ProfileUser[]>(`/api/search/${debouncedKeyword}`);
 
-  return <></>;
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+  return (
+    <section className="w-full max-w-2xl my-4 flex flex-col items-center">
+      <form className="w-full mb-4" onSubmit={onSubmit}>
+        <input
+          className="w-full text-xl p-3 outline-none border border-gray-400"
+          type="text"
+          autoFocus
+          placeholder="Search for a username or name"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </form>
+      {error && <p>무언가가 잘못 되었음 😜</p>}
+      {isLoading && <BeatLoader />}
+      {!isLoading && !error && users?.length === 0 && (
+        <p>찾는 사용자가 없음 😭</p>
+      )}
+      <ul className="w-full p-4">
+        {users &&
+          users.map((user) => (
+            <li key={user.username}>
+              <UserCard user={user} />
+            </li>
+          ))}
+      </ul>
+    </section>
+  );
 }
