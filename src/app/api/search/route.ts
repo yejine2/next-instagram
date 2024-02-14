1928c845b3e87a16fb04dac8989822b02b062287
yejine2 <yejine2@naver.com>
2024-02-14 14:23:15 +0900

    feat: Add dynamic

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,5 +1,7 @@
-import { searchUsers } from '@/service/user';
-import { NextResponse } from 'next/server';
+import { searchUsers } from "@/service/user";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   return searchUsers().then((data) => NextResponse.json(data));
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,6 @@
 import UserSearch from "@/components/UserSearch";
 import { Metadata } from "next";
-
+export const dynamic = "force-dynamic";
 export const metadata: Metadata = {
   title: "User Search",
   description: "Search user to follow",
