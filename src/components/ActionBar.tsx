b7a4c6a2c4f8069c7f56263fe57c992001645c73
yejine2 <yejine2@naver.com>
2024-03-22 12:42:13 +0900

    feat: Update ActionBar

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -6,21 +6,32 @@ import { useState } from "react";
 import HeartFillIcon from "./ui/icons/HeartFillIcon";
 import BookmarkFillIcon from "./ui/icons/BookmarkFillIcon";
 import ToggleButton from "./ui/ToggleButton";
+import { SimplePost } from "@/model/post";
+import { useSession } from "next-auth/react";
 type Props = {
-  likes: string[];
-  username: string;
-  createdAt: string;
-  text?: string;
+  post: SimplePost;
 };
-export default function ActionBar({ likes, username, text, createdAt }: Props) {
-  const [liked, setLiked] = useState(false);
+export default function ActionBar({ post }: Props) {
+  const { id, likes, username, text, createdAt } = post;
+  const { data: session } = useSession();
+  const user = session?.user;
+
+  const [liked, setLiked] = useState(
+    user ? likes.includes(user.username) : false
+  );
   const [bookmarked, setBookmarked] = useState(false);
+  const handleLike = (like: boolean) => {
+    fetch("api/likes", {
+      method: "PUT",
+      body: JSON.stringify({ id, like }),
+    }).then(() => setLiked(like));
+  };
   return (
     <>
       <div className="flex justify-between my-2 px-4">
         <ToggleButton
           toggled={liked}
-          onToggle={setLiked}
+          onToggle={handleLike}
           onIcon={<HeartFillIcon />}
           offIcon={<HeartIcon />}
         />
--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -50,7 +50,7 @@ export default function PostDetail({ post }: Props) {
               )
             )}
         </ul>
-        <ActionBar likes={likes} username={username} createdAt={createdAt} />
+        <ActionBar post={post} />
         <CommentForm />
       </div>
     </section>
