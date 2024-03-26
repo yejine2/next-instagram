3ca7bdf20d08311738233f0ffce8aa15e251359f
yejine2 <yejine2@naver.com>
2024-03-26 15:15:52 +0900

    feat: Create service follow, unfollow

--- src/service/user.ts
+++ src/service/user.ts
@@ -69,7 +69,11 @@ export async function getUserForProfile(username: string) {
       "followers": count(followers),
       "posts": count(*[_type=="post" && author->username == "${username}"])
     }
-    `
+    `,
+      undefined,
+      {
+        cache: "no-store",
+      }
     )
     .then((user) => ({
       ...user,
@@ -97,4 +101,29 @@ export async function removeBookmark(userId: string, postId: string) {
     .patch(userId)
     .unset([`bookmarks[_ref=="${postId}"]`])
     .commit();
+}
+
+// https://www.sanity.io/docs/js-client#multiple-mutations-in-a-transaction
+export async function follow(myId: string, targetId: string) {
+  return client
+    .transaction()
+    .patch(myId, (user) =>
+      user
+        .setIfMissing({ following: [] })
+        .append("following", [{ _ref: targetId, _type: "reference" }])
+    )
+    .patch(targetId, (user) =>
+      user
+        .setIfMissing({ followers: [] })
+        .append("followers", [{ _ref: myId, _type: "reference" }])
+    )
+    .commit({ autoGenerateArrayKeys: true });
+}
+
+export async function unfollow(myId: string, targetId: string) {
+  return client
+    .transaction() //
+    .patch(myId, (user) => user.unset([`following[_ref=="${targetId}"]`]))
+    .patch(targetId, (user) => user.unset([`followers[_ref=="${myId}"]`]))
+    .commit({ autoGenerateArrayKeys: true });
 }
